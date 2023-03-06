2a9f41f41e84abba482fe213d79c1be5b08c2f1d
anstice <ko.an.2018@yandex.ru>
2023-03-06 16:08:49 +0300

    task 43 completed

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -14,6 +14,6 @@ double k1 = Number("Введите координату k1: ");
 double b2 = Number("Введите координату b2: ");
 double k2 = Number("Введите координату k2: ");
 
-double resX = (-b2 + b1)/(-k1 + k2);
+double resX = (b2 - b1)/(k1 - k2);
 double resY = k2 * resX + b2;
 Console.WriteLine($"Пересечение двух прямых произойдет в точке с координатами x = {Math.Round(resX, 3)}; y = {Math.Round(resY, 3)}");
